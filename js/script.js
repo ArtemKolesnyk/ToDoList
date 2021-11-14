c2aeaabd8f3611001841d72b97eed29402e8a37f
ArtemKolesnyk <kolesnik2405@gmail.com>
2021-11-14 22:42:16 +0100

    todolist

--- js/script.js
+++ js/script.js
@@ -1,62 +1,101 @@
 const inputRef = document.getElementById('myInput');
 const buttonRef = document.querySelector('.addBtn');
 const ulRef = document.getElementById('myUL');
+const closeRef = document.getElementsByClassName("close");
 
 
-buttonRef.addEventListener('click', onClick);
-ulRef.addEventListener('click', upDateChecked);
+buttonRef.addEventListener('click', addTask);
+ulRef.addEventListener('click', updateChecked);
+ulRef.addEventListener('click', complitedTask);
+closeRef.onclick
 
-
-function onClick() {
+function addTask() {
     const value = inputRef.value.trim();
-    if(!value) {
+    if(!value) {   
         return false;
     }
-    onSave(value);
-    markup();
+    saveTask(value);
+    createMarkup();
     inputRef.value = '';
 }
 
-function onSave(value) {
-    const obj = {
+function saveTask(value) {
+    const objTask = {
         text: value,
         checked: false,
     };
-    let data = localStorage.getItem('list') || [];
+    let data = localStorage.getItem('listTask') || [];
     if(Array.isArray(data)) {
-        data.push(obj)
+        data.push(objTask)
     } else {data = JSON.parse(data);
-    data.push(obj);
+    data.push(objTask);
     }
-    
-    localStorage.setItem('list', JSON.stringify(data));
+    localStorage.setItem('listTask', JSON.stringify(data));
 }
 
-function markup() {
-    let data = localStorage.getItem('list');
-    if(!data) return;
-    data = JSON.parse(data);
-    const addMarkup = data.map(({text, checked}) => {
+function createMarkup() {
+    let data = localStorage.getItem('listTask');
+    if(!data) {
+        return;
+    } else {
+        data = JSON.parse(data);
+    const addTask = data.map(({text, checked}) => {
         const classElem = !!checked ? 'checked': '';
-        return `<li class="${classElem}">${text}</li>`;
+        const elClose = '\u00D7';
+        const classClose = 'close';
+        return `<li class='${classElem}'>${text}
+        <span class='${classClose}'>${elClose}</span></li>`;
     }).join('');
-    ulRef.innerHTML = addMarkup;
+    ulRef.innerHTML = addTask;
+    }
 }
-markup();
+createMarkup();
 
-function upDateChecked({target}) {
+function updateChecked({target}) {
     if(target.tageName !== 'LI') return;
     const text = target.textContent;
-    localStorage.getItem('list');
+    localStorage.getItem('listTask');
     data = JSON.parse(data);
     for(const d in data) {
         if(data[d].text === text) {
             data[d].checked = !data[d].checked;
         }
-        localStorage.setItem('list', JSON.stringify(data));
-        markup();
+        localStorage.setItem('listTask', JSON.stringify(data)); 
     }
 }
+createMarkup();
+
+
+function complitedTask(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}
+
+for ( let i = 0; i < closeRef.length; i++) {
+  closeRef[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
